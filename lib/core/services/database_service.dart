c502bb867c6a6c0f4c6e86a2fed03795b8f275b3
Sioxox <sioxox@pm.me>
2024-08-06 22:56:56 +0200

    Test database credentials (decrepated)

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -2,5 +2,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> initializeSupabase() async {
   await Supabase.initialize(
+    url: 'https://phpswydqmicwjplsjjyq.supabase.co/',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBocHN3eWRxbWljd2pwbHNqanlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI5NjE1MjQsImV4cCI6MjAzODUzNzUyNH0.xEeT7Xi_IgXa5LAD8HncCyFPPcTV-Wk_3qJjmIG61LM',
   );
 }
