13e419507e590c22656031ffc30e996db619f88a
Sioxox <sioxox@pm.me>
2024-08-07 01:47:34 +0200

    Moving languages capabilities in right files

---
+++ lib/features/settings/settings.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sway_events/core/constants/l10n.dart';
+
+class Settings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            final newLang = await _showLanguageDialog(context);
+            if (newLang != null) {
+              context.read<LanguageCubit>().changeLang(context, newLang);
+            }
+          },
+          child: const Text('Change Language'),
+        ),
+      ),
+    );
+  }
+
+  Future<String?> _showLanguageDialog(BuildContext context) {
+    return showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Select Language'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                title: const Text('English'),
+                onTap: () => Navigator.pop(context, 'en'),
+              ),
+              ListTile(
+                title: const Text('French'),
+                onTap: () => Navigator.pop(context, 'fr'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sway_events/core/constants/app_theme.dart';
 import 'package:sway_events/core/constants/l10n.dart';
 import 'package:sway_events/core/routes.dart';
@@ -46,24 +45,5 @@ class SwayEvents extends StatelessWidget {
         },
       ),
     );
-  }
-}
-
-class LanguageCubit extends Cubit<Locale> {
-  LanguageCubit() : super(const Locale('en', ''));
-
-  Future<void> chargeStartLang() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String? langCode = prefs.getString('lang');
-    debugPrint(langCode);
-    if (langCode != null) {
-      emit(Locale(langCode, ''));
-    }
-  }
-
-  Future<void> changeLang(BuildContext context, String data) async {
-    emit(Locale(data, ''));
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('lang', data);
   }
 }
