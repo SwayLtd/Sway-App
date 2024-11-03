140fb9f7af09399873eb090424af75de35808378
Sioxox <21986104+regisandre@users.noreply.github.com>
2024-11-03 04:17:46 +0100

    Update of FollowingButton on click

--- lib/features/user/widgets/following_button_widget.dart
+++ lib/features/user/widgets/following_button_widget.dart
@@ -1,4 +1,4 @@
-// following_button_widget.dart
+// lib/features/user/widgets/following_button_widget.dart
 
 import 'package:flutter/material.dart';
 import 'package:sway/features/user/services/user_follow_artist_service.dart';
@@ -7,92 +7,148 @@ import 'package:sway/features/user/services/user_follow_promoter_service.dart';
 import 'package:sway/features/user/services/user_follow_user_service.dart';
 import 'package:sway/features/user/services/user_follow_venue_service.dart';
 
-class FollowingButtonWidget extends StatelessWidget {
+class FollowingButtonWidget extends StatefulWidget {
   final int entityId;
   final String entityType; // 'venue', 'promoter', 'artist', 'user', 'genre'
 
-  const FollowingButtonWidget(
-      {required this.entityId, required this.entityType,});
+  const FollowingButtonWidget({
+    required this.entityId,
+    required this.entityType,
+  });
+
+  @override
+  _FollowingButtonWidgetState createState() => _FollowingButtonWidgetState();
+}
+
+class _FollowingButtonWidgetState extends State<FollowingButtonWidget> {
+  bool isFollowing = false;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFollowingStatus();
+  }
+
+  Future<void> _loadFollowingStatus() async {
+    try {
+      bool following = await _isFollowing();
+      setState(() {
+        isFollowing = following;
+        isLoading = false;
+      });
+    } catch (e) {
+      print('Error loading following status: $e');
+      setState(() {
+        isFollowing = false;
+        isLoading = false;
+      });
+    }
+  }
 
   Future<bool> _isFollowing() {
-    switch (entityType) {
+    switch (widget.entityType) {
       case 'artist':
-        return UserFollowArtistService().isFollowingArtist(entityId);
+        return UserFollowArtistService().isFollowingArtist(widget.entityId);
       case 'venue':
-        return UserFollowVenueService().isFollowingVenue(entityId);
+        return UserFollowVenueService().isFollowingVenue(widget.entityId);
       case 'promoter':
-        return UserFollowPromoterService().isFollowingPromoter(entityId);
+        return UserFollowPromoterService().isFollowingPromoter(widget.entityId);
       case 'genre':
-        return UserFollowGenreService().isFollowingGenre(entityId);
+        return UserFollowGenreService().isFollowingGenre(widget.entityId);
       case 'user':
-        return UserFollowUserService().isFollowingUser(entityId);
+        return UserFollowUserService().isFollowingUser(widget.entityId);
       default:
         throw Exception('Unknown entity type');
     }
   }
 
-  Future<void> _followEntity() {
-    switch (entityType) {
+  Future<void> _followEntity() async {
+    switch (widget.entityType) {
       case 'artist':
-        return UserFollowArtistService().followArtist(entityId);
+        await UserFollowArtistService().followArtist(widget.entityId);
+        break;
       case 'venue':
-        return UserFollowVenueService().followVenue(entityId);
+        await UserFollowVenueService().followVenue(widget.entityId);
+        break;
       case 'promoter':
-        return UserFollowPromoterService().followPromoter(entityId);
+        await UserFollowPromoterService().followPromoter(widget.entityId);
+        break;
       case 'genre':
-        return UserFollowGenreService().followGenre(entityId);
+        await UserFollowGenreService().followGenre(widget.entityId);
+        break;
       case 'user':
-        return UserFollowUserService().followUser(entityId);
+        await UserFollowUserService().followUser(widget.entityId);
+        break;
       default:
         throw Exception('Unknown entity type');
     }
   }
 
-  Future<void> _unfollowEntity() {
-    switch (entityType) {
+  Future<void> _unfollowEntity() async {
+    switch (widget.entityType) {
       case 'artist':
-        return UserFollowArtistService().unfollowArtist(entityId);
+        await UserFollowArtistService().unfollowArtist(widget.entityId);
+        break;
       case 'venue':
-        return UserFollowVenueService().unfollowVenue(entityId);
+        await UserFollowVenueService().unfollowVenue(widget.entityId);
+        break;
       case 'promoter':
-        return UserFollowPromoterService().unfollowPromoter(entityId);
+        await UserFollowPromoterService().unfollowPromoter(widget.entityId);
+        break;
       case 'genre':
-        return UserFollowGenreService().unfollowGenre(entityId);
+        await UserFollowGenreService().unfollowGenre(widget.entityId);
+        break;
       case 'user':
-        return UserFollowUserService().unfollowUser(entityId);
+        await UserFollowUserService().unfollowUser(widget.entityId);
+        break;
       default:
         throw Exception('Unknown entity type');
+    }
+  }
+
+  Future<void> _toggleFollow() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      if (isFollowing) {
+        await _unfollowEntity();
+      } else {
+        await _followEntity();
+      }
+      bool updatedStatus = await _isFollowing();
+      setState(() {
+        isFollowing = updatedStatus;
+        isLoading = false;
+      });
+    } catch (e) {
+      print('Error toggling follow status: $e');
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: _isFollowing(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          final bool isFollowing = snapshot.data ?? false;
-          return IconButton(
-            icon: Icon(
-              isFollowing ? Icons.star : Icons.star_border,
-              color: isFollowing ? Colors.yellow : Theme.of(context).iconTheme.color,
-            ),
-            onPressed: () async {
-              if (isFollowing) {
-                await _unfollowEntity();
-              } else {
-                await _followEntity();
-              }
-              // Refresh UI after follow/unfollow
-              (context as Element).markNeedsBuild();
-            },
-          );
-        }
-      },
-    );
+    if (isLoading) {
+      return const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: SizedBox(
+          width: 24,
+          height: 24,
+          child: CircularProgressIndicator(strokeWidth: 2),
+        ),
+      );
+    } else {
+      return IconButton(
+        icon: Icon(
+          isFollowing ? Icons.star : Icons.star_border,
+          // You can customize the color if needed
+        ),
+        onPressed: _toggleFollow,
+      );
+    }
   }
 }
