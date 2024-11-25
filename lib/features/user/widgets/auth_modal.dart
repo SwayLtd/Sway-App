06d4f22c65428b3807ded98c8790e74555ecdf1c
Sioxox <21986104+regisandre@users.noreply.github.com>
2024-11-25 02:40:14 +0100

    Fix auth modal size for sign up

--- lib/features/user/widgets/auth_modal.dart
+++ lib/features/user/widgets/auth_modal.dart
@@ -14,7 +14,8 @@ class AuthModal extends StatelessWidget {
     showModalBottomSheet(
       context: context,
       isScrollControlled:
-          true, // Pour ajuster la hauteur en fonction du contenu
+          true, // Permet au modal de s'ajuster en fonction du contenu et du clavier
+      useSafeArea: true, // Évite les chevauchements avec les zones sensibles
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
       ),
@@ -43,12 +44,12 @@ class AuthModal extends StatelessWidget {
     return null;
   }
 
-  /// Local username validator
+  /// Validateur de nom d'utilisateur local
   String? _usernameValidator(String? username) {
     if (username == null || username.isEmpty) {
       return 'Please enter a username.';
     }
-    // Regular expression to allow letters, numbers, dots, and underscores
+    // Expression régulière pour permettre lettres, chiffres, points et underscores
     final usernameRegex = RegExp(r'^[a-zA-Z0-9._]+$');
     if (!usernameRegex.hasMatch(username)) {
       return 'Username can only contain letters, numbers, dots, and underscores.';
@@ -64,105 +65,148 @@ class AuthModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          // Barre grise horizontale
-          Container(
-            height: 5,
-            width: 50,
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            decoration: BoxDecoration(
-              color: Colors.grey[300],
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              bottom: MediaQuery.of(context).viewInsets.bottom,
-              left: 16.0,
-              right: 16.0,
-              top: 16.0,
-            ),
-            child: SupaEmailAuth(
-              // URL de redirection pour les liens magiques
-              redirectTo: kIsWeb ? null : 'app.sway.main://login-callback/',
-              // Callback après une connexion réussie
-              onSignInComplete: (response) {
-                if (response.session != null) {
-                  Navigator.of(context).pop();
-                }
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(behavior: SnackBarBehavior.floating, content: Text('Signed in successfully')),
-                );
-              },
-              // Callback après une inscription réussie
-              onSignUpComplete: (response) {
-                if (response.session != null) {
-                  Navigator.of(context).pop();
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                        content: Text('Please confirm your signup by email')),
-                  );
-                }
-              },
-              // Validateur de mot de passe personnalisé
-              passwordValidator: _passwordValidator,
-              // URL de redirection après réinitialisation du mot de passe
-              resetPasswordRedirectTo:
-                  kIsWeb ? null : 'app.sway.main://reset-password/',
-              // Champs de métadonnées pour capturer des informations supplémentaires lors de l'inscription
-              metadataFields: [
-                MetaDataField(
-                  prefixIcon: const Icon(Icons.person),
-                  label: 'Username',
-                  key: 'username',
-                  validator: _usernameValidator,
-                ),
-                BooleanMetaDataField(
-                  label: 'I wish to receive marketing emails',
-                  key: 'marketing_consent',
-                  checkboxPosition: ListTileControlAffinity.leading,
-                ),
-                BooleanMetaDataField(
-                  key: 'terms_agreement',
-                  isRequired: true,
-                  checkboxPosition: ListTileControlAffinity.leading,
-                  richLabelSpans: [
-                    const TextSpan(text: 'I have read and agree to the '),
-                    TextSpan(
-                      text: 'Terms and Conditions',
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) =>
-                                    const TermsAndConditionsScreen()),
-                          );
-                        },
+    // Définir une hauteur maximale pour le modal (75% de la hauteur de l'écran)
+    final double maxHeight = MediaQuery.of(context).size.height * 0.43;
+
+    return SafeArea(
+      child: Container(
+        constraints: BoxConstraints(
+          maxHeight: maxHeight,
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Column(
+          children: [
+            // Barre grise horizontale centrée et bouton de fermeture
+            Stack(
+              children: [
+                // Barre grise horizontale centrée
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    height: 5,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                  ],
+                  ),
+                ),
+                // Bouton de fermeture (croix) en haut à droite
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
                 ),
               ],
-              // Gestion des erreurs
-              onError: (error) {
-                String errorMessage = 'An unknown error occurred.';
-                if (error is AuthException) {
-                  errorMessage = error.message;
-                } else if (error is Exception) {
-                  errorMessage = error.toString();
-                }
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(behavior: SnackBarBehavior.floating, content: Text('Error: $errorMessage')),
-                );
-              },
             ),
-          ),
-        ],
+            const SizedBox(height: 10),
+            // Contenu défilable
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom,
+                    left: 16.0,
+                    right: 16.0,
+                    top: 16.0,
+                  ),
+                  child: SupaEmailAuth(
+                    // URL de redirection pour les liens magiques
+                    redirectTo:
+                        kIsWeb ? null : 'app.sway.main://login-callback/',
+                    // Callback après une connexion réussie
+                    onSignInComplete: (response) {
+                      if (response.session != null) {
+                        Navigator.of(context).pop();
+                      }
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          behavior: SnackBarBehavior.floating,
+                          content: Text('Signed in successfully'),
+                        ),
+                      );
+                    },
+                    // Callback après une inscription réussie
+                    onSignUpComplete: (response) {
+                      if (response.session != null) {
+                        Navigator.of(context).pop();
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content:
+                                Text('Please confirm your signup by email'),
+                          ),
+                        );
+                      }
+                    },
+                    // Validateur de mot de passe personnalisé
+                    passwordValidator: _passwordValidator,
+                    // URL de redirection après réinitialisation du mot de passe
+                    resetPasswordRedirectTo:
+                        kIsWeb ? null : 'app.sway.main://reset-password/',
+                    // Champs de métadonnées pour capturer des informations supplémentaires lors de l'inscription
+                    metadataFields: [
+                      MetaDataField(
+                        prefixIcon: const Icon(Icons.person),
+                        label: 'Username',
+                        key: 'username',
+                        validator: _usernameValidator,
+                      ),
+                      BooleanMetaDataField(
+                        label: 'I wish to receive marketing emails',
+                        key: 'marketing_consent',
+                        value: true,
+                        checkboxPosition: ListTileControlAffinity.leading,
+                      ),
+                      BooleanMetaDataField(
+                        key: 'terms_agreement',
+                        isRequired: true,
+                        checkboxPosition: ListTileControlAffinity.leading,
+                        richLabelSpans: [
+                          const TextSpan(text: 'I have read and agree to the '),
+                          TextSpan(
+                            text: 'Terms and Conditions',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const TermsAndConditionsScreen()),
+                                );
+                              },
+                          ),
+                        ],
+                      ),
+                    ],
+                    // Gestion des erreurs
+                    onError: (error) {
+                      String errorMessage = 'An unknown error occurred.';
+                      if (error is AuthException) {
+                        errorMessage = error.message;
+                      } else if (error is Exception) {
+                        errorMessage = error.toString();
+                      }
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          behavior: SnackBarBehavior.floating,
+                          content: Text('Error: $errorMessage'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
