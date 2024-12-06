2628ae959b4331ae187e36fbf0c523736dc6593b
Sioxox <21986104+regisandre@users.noreply.github.com>
2024-12-06 02:52:47 +0100

    Update auth_state_manager.dart

--- lib/core/auth_state_manager.dart
+++ lib/core/auth_state_manager.dart
@@ -2,6 +2,7 @@
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthStateManager extends ChangeNotifier {
@@ -13,20 +14,25 @@ class AuthStateManager extends ChangeNotifier {
 
   Future<void> _setFcmToken(String? fcmToken) async {
     if (fcmToken == null) return; // Vérifiez si le token n'est pas nul
-    final userId = supabase.auth.currentUser?.id;
+    final supabaseId = supabase.auth.currentUser?.id;
 
-    if (userId != null) {
+    if (supabaseId != null) {
+      print('Upserting FCM token for supabase_id: $supabaseId');
       try {
-        await supabase.from('users').upsert({
-          'supabase_id': userId,
-          'fcm_token': fcmToken,
-        },
-            onConflict:
-                'supabase_id'); // Assurez-vous que 'supabase_id' est une clé unique
+        await supabase.from('users').upsert(
+          {
+            'supabase_id': supabaseId,
+            'fcm_token': fcmToken,
+          },
+          onConflict:
+              'supabase_id', // Assurez-vous que 'supabase_id' est une clé unique
+        );
+        print('FCM token upserted successfully.');
       } catch (e) {
-        // Gérer les erreurs, peut-être afficher un SnackBar
         print('FCM token upsert error: $e');
       }
+    } else {
+      print('No authenticated user found. Skipping FCM token upsert.');
     }
   }
 
@@ -41,12 +47,26 @@ class AuthStateManager extends ChangeNotifier {
         await FirebaseMessaging.instance.getAPNSToken();
         final fcmToken =
             await FirebaseMessaging.instance.getToken(); // Utilisez await ici
-        await _setFcmToken(fcmToken);
+        if (fcmToken != null) {
+          await _setFcmToken(fcmToken);
+        }
       }
     });
 
     FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {
       _setFcmToken(fcmToken);
+    });
+
+    FirebaseMessaging.onMessage.listen((payload) {
+      final notification = payload.notification;
+
+      if (notification != null) {
+        /* ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              behavior: SnackBarBehavior.floating,
+              content: Text('${notification.title} ${notification.body}')),
+        ); */
+      }
     });
   }
 }
