70e3fa2afd4fd66d3e1359fc0ac5f78059a8cc01
Sioxox <21986104+regisandre@users.noreply.github.com>
2025-01-01 23:12:18 +0100

    Alternation between the next event and the number of future events on artist widget

--- lib/features/artist/widgets/artist_item_widget.dart
+++ lib/features/artist/widgets/artist_item_widget.dart
@@ -1,12 +1,16 @@
 // lib/features/artist/widgets/artist_item_widget.dart
 
+import 'dart:async'; // Importer pour Timer
 import 'package:flutter/material.dart';
 import 'package:sway/core/constants/dimensions.dart';
+import 'package:sway/core/utils/date_utils.dart'; // Importer les utilitaires de date
 import 'package:sway/core/widgets/image_with_error_handler.dart';
 import 'package:sway/features/artist/models/artist_model.dart';
+import 'package:sway/features/event/models/event_model.dart';
 import 'package:sway/features/event/services/event_artist_service.dart';
 import 'package:sway/features/user/services/user_follow_artist_service.dart';
 import 'package:sway/features/user/widgets/following_button_widget.dart';
+import 'package:go_router/go_router.dart'; // Importer GoRouter pour la navigation
 
 /// Widget pour afficher un artiste sous forme de liste avec une image, un titre limité,
 /// un bouton "follow", le nombre de followers et les événements à venir.
@@ -28,20 +32,94 @@ class ArtistListItemWidget extends StatefulWidget {
 
 class _ArtistListItemWidgetState extends State<ArtistListItemWidget> {
   late Future<int> _followersCountFuture;
-  late Future<int> _upcomingEventsCountFuture;
+  late Future<List<Event>> _upcomingEventsFuture;
 
   final UserFollowArtistService _userFollowArtistService =
       UserFollowArtistService();
   final EventArtistService _eventArtistService = EventArtistService();
+
+  bool _showUpcomingEventsCount = true; // État pour l'alternance
+  Timer? _timer; // Timer pour l'alternance
+  bool _hasTimer =
+      false; // Indicateur pour éviter de configurer plusieurs timers
 
   @override
   void initState() {
     super.initState();
     _followersCountFuture =
         _userFollowArtistService.getArtistFollowersCount(widget.artist.id);
-    _upcomingEventsCountFuture = _eventArtistService
-        .getEventsByArtistId(widget.artist.id)
-        .then((events) => events.length);
+    _upcomingEventsFuture =
+        _fetchUpcomingEvents(); // Récupérer la liste des événements à venir
+  }
+
+  /// Méthode pour récupérer la liste des événements à venir en utilisant getEventsByArtistId
+  Future<List<Event>> _fetchUpcomingEvents() async {
+    try {
+      final List<Map<String, dynamic>> eventsData =
+          await _eventArtistService.getEventsByArtistId(widget.artist.id);
+
+      if (eventsData.isEmpty) {
+        print('Aucun événement trouvé pour l\'artiste ID: ${widget.artist.id}');
+        return [];
+      }
+
+      // Extraire les objets Event à partir des maps retournées
+      List<Event> events = eventsData
+          .map((data) {
+            final event =
+                data['event'] as Event?; // Traiter 'event' comme un objet Event
+            if (event == null) {
+              print(
+                  'Données de l\'événement manquantes pour un événement associé à l\'artiste ID: ${widget.artist.id}');
+              return null;
+            }
+            return event;
+          })
+          .where((event) => event != null)
+          .cast<Event>()
+          .toList();
+
+      // Filtrer les événements à venir
+      DateTime now = DateTime.now();
+      List<Event> upcomingEvents =
+          events.where((event) => event.dateTime.isAfter(now)).toList();
+
+      print(
+          'Nombre d\'événements à venir pour l\'artiste ID: ${widget.artist.id} est ${upcomingEvents.length}');
+
+      if (upcomingEvents.isEmpty) {
+        return [];
+      }
+
+      // Trier les événements par date ascendante
+      upcomingEvents.sort((a, b) => a.dateTime.compareTo(b.dateTime));
+
+      print(
+          'Prochain événement: ${upcomingEvents.first.title} le ${upcomingEvents.first.dateTime}');
+
+      // Configurer le timer pour l'alternance si non déjà configuré
+      if (!_hasTimer) {
+        _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+          setState(() {
+            _showUpcomingEventsCount = !_showUpcomingEventsCount;
+          });
+        });
+        _hasTimer = true;
+      }
+
+      return upcomingEvents;
+    } catch (e) {
+      // Gérer les erreurs ici ou les laisser remonter
+      print('Erreur lors de la récupération des événements: $e');
+      return [];
+    }
+  }
+
+  @override
+  void dispose() {
+    // Annuler le timer si actif
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
@@ -66,7 +144,7 @@ class _ArtistListItemWidgetState extends State<ArtistListItemWidget> {
               color: Theme.of(context)
                   .colorScheme
                   .onPrimary
-                  .withValues(alpha: 0.5), // Couleur de la bordure avec opacité
+                  .withOpacity(0.5), // Couleur de la bordure avec opacité
               width: 2.0, // Épaisseur de la bordure
             ),
             borderRadius:
@@ -86,7 +164,7 @@ class _ArtistListItemWidgetState extends State<ArtistListItemWidget> {
                     color: Theme.of(context)
                         .colorScheme
                         .onPrimary
-                        .withValues(alpha: 0.5), // Couleur de la bordure
+                        .withOpacity(0.5), // Couleur de la bordure
                     width: 2.0, // Épaisseur de la bordure
                   ),
                   borderRadius:
@@ -115,12 +193,13 @@ class _ArtistListItemWidgetState extends State<ArtistListItemWidget> {
             subtitle: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                // Affichage du nombre de followers
                 FutureBuilder<int>(
                   future: _followersCountFuture,
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
                       return const Text(
-                        '0 followers',
+                        'Loading followers...',
                         style: TextStyle(fontSize: 12, color: Colors.grey),
                       );
                     } else if (snapshot.hasError) {
@@ -128,6 +207,11 @@ class _ArtistListItemWidgetState extends State<ArtistListItemWidget> {
                         'Error: ${snapshot.error}',
                         style:
                             const TextStyle(fontSize: 12, color: Colors.grey),
+                      );
+                    } else if (!snapshot.hasData) {
+                      return const Text(
+                        '0 followers',
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
                       );
                     } else {
                       return Text(
@@ -138,12 +222,14 @@ class _ArtistListItemWidgetState extends State<ArtistListItemWidget> {
                     }
                   },
                 ),
-                FutureBuilder<int>(
-                  future: _upcomingEventsCountFuture,
+                const SizedBox(height: 4), // Espacement entre les sections
+                // Affichage des événements à venir
+                FutureBuilder<List<Event>>(
+                  future: _upcomingEventsFuture,
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
                       return const Text(
-                        '0 upcoming events', // 'Loading events...',
+                        'Loading upcoming events...',
                         style: TextStyle(fontSize: 12, color: Colors.grey),
                       );
                     } else if (snapshot.hasError) {
@@ -152,11 +238,76 @@ class _ArtistListItemWidgetState extends State<ArtistListItemWidget> {
                         style:
                             const TextStyle(fontSize: 12, color: Colors.grey),
                       );
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      // Si aucun événement à venir, afficher "No upcoming events"
+                      // Annuler le timer si actif
+                      if (_hasTimer) {
+                        _timer?.cancel();
+                        _timer = null;
+                        _hasTimer = false;
+                      }
+                      return const Text(
+                        'No upcoming events',
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      );
                     } else {
-                      return Text(
-                        '${snapshot.data} upcoming events',
-                        style:
-                            const TextStyle(fontSize: 12, color: Colors.grey),
+                      final upcomingEvents = snapshot.data!;
+                      print(
+                          'Upcoming Events List: $upcomingEvents'); // Log pour débogage
+                      // Vérifier que la liste n'est pas vide
+                      if (upcomingEvents.isEmpty) {
+                        // Si la liste est vide, afficher "No upcoming events"
+                        if (_hasTimer) {
+                          _timer?.cancel();
+                          _timer = null;
+                          _hasTimer = false;
+                        }
+                        return const Text(
+                          'No upcoming events',
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        );
+                      }
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Affichage conditionnel alterné entre "X upcoming events" et "Next Event"
+                          _showUpcomingEventsCount
+                              ? Text(
+                                  '${upcomingEvents.length} upcoming event${upcomingEvents.length > 1 ? 's' : ''}',
+                                  style: const TextStyle(
+                                      fontSize: 12, color: Colors.grey),
+                                )
+                              : GestureDetector(
+                                  onTap: () {
+                                    // Naviguer vers la page de détails de l'événement
+                                    context.push(
+                                        '/event/${upcomingEvents.first.id}');
+                                  },
+                                  child: Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.navigate_next,
+                                        size: 16,
+                                        color: Colors.red,
+                                      ),
+                                      const SizedBox(width: 2),
+                                      Expanded(
+                                        child: Text(
+                                          '${upcomingEvents.first.title} on ${formatEventDate(upcomingEvents.first.dateTime)}',
+                                          style: const TextStyle(
+                                            fontSize: 12,
+                                            color: Colors.grey, // Texte neutre
+                                            // Pas de soulignement
+                                          ),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                          // Ajouter un espace entre les deux éléments si nécessaire
+                        ],
                       );
                     }
                   },
@@ -208,7 +359,7 @@ class ArtistCardItemWidget extends StatelessWidget {
                   color: Theme.of(context)
                       .colorScheme
                       .onPrimary
-                      .withValues(alpha: 0.5), // Couleur de la bordure
+                      .withOpacity(0.5), // Couleur de la bordure
                   width: 2.0, // Épaisseur de la bordure
                 ),
                 borderRadius:
@@ -228,7 +379,7 @@ class ArtistCardItemWidget extends StatelessWidget {
             Text(
               truncatedName,
               style: const TextStyle(fontSize: 14),
-              maxLines: 1, // Permet jusqu'à 2 lignes
+              maxLines: 1, // Permet jusqu'à 1 ligne
               overflow:
                   TextOverflow.ellipsis, // Ajoute des ellipses si nécessaire
               textAlign: TextAlign.center, // Optionnel : centre le texte
