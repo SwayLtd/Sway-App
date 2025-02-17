021f60c701990f51220cf01dbcd044222ccc5c0a
Sioxox <21986104+regisandre@users.noreply.github.com>
2025-02-17 15:27:46 +0100

    Remove entity name when sharing

--- lib/core/utils/share_util.dart
+++ lib/core/utils/share_util.dart
@@ -1,8 +1,9 @@
-// share_util.dart
+// lib/core/utils/share_util.dart
 
 import 'package:share_plus/share_plus.dart';
 
 void shareEntity(String entityType, int entityId, String entityName) {
   final url = 'https://sway.events/$entityType/$entityId';
-  Share.share('$entityName\n$url');
+  // Share.share('$entityName/n$url');
+  Share.share('$url');
 }
